0543dcd7559dda1e74d1dfb58eeb3176f38fe003
Cendekia Astian <cendiastian@gmail.com>
2022-01-04 11:56:47 +0700

    create theme & configure redux on index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
-import Regist from "./pages/register";
-// import Register from "./pages/regist";
+import Register from "./pages/register";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  components: {
+    Container: {
+      variants: [
+        {
+          props: { variant: "login" },
+          style: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            color: "white",
+            maxWidth: "fit-content",
+            backgroundColor: "#212121",
+            borderRadius: 15,
+            padding: (20, 20, 20, 20),
+            mt: 2,
+          },
+        },
+      ],
+    },
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#25274D",
+      typography: "#fffff",
+      button: "#4A598E",
+    },
+    secondary: {
+      main: "#0D5C8C",
+      button: "#2C272E",
+      typography: "#fffff",
+    },
+    typography: {
+      main: "#fffff",
+    },
+    bg: {
+      main: "#272727",
+    },
+    outline: {
+      main: "#ABABB1",
+    },
+    danger: {
+      main: "#95150c",
+      dark: "#f44336",
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Regist />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
