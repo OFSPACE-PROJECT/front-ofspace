9ea616ac4cdba87abfad67a8be4078336fde3635
Cendekia Astian <cendiastian@gmail.com>
2022-01-19 11:06:23 +0700

    Update chat and message components

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,11 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import {
-  Typography,
-  Container,
-  InputBase,
-  IconButton,
-} from "@mui/material";
+import { Typography, Container, InputBase, IconButton } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useSelector } from "react-redux";
 import SendIcon from "@mui/icons-material/Send";
@@ -14,82 +9,95 @@ import { useInsertMessage } from "../../hooks/InsertMessage";
 import { useSubscriptionMessage } from "../../hooks/SubscriptionMessage";
 import { useSubscriptionChat } from "../../hooks/SubscriptionChat";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    color: "white",
-    width: 800,
-    backgroundColor: "#212121",
-    borderRadius: 15,
-    padding: (20, 20, 20, 20),
-  },
-});
-export default function Chat() {
+export default function Chat(data) {
   const user = useSelector((state) => state.persistedReducer.user);
   console.log(user);
-  // console.log(user.id);
   console.log(user.role);
-  const styles = useStyles();
   const [value, setValue] = useState("");
-  const [param, setParam] = useState({});
-  const { InsertMessage  } = useInsertMessage();
-  const { allChat, errorAllChat, loadingAllChat } = useSubscriptionChat(param);
-  useEffect(() => {
-    if (user) {
-      if (user.role === "customer") {
-        setParam({ customer_id: { _eq: user.id } });
-      } else {
-        setParam({ consultan_id: { _eq: user.id } });
-      }
-      console.log(param);
-    }
-  }, [user]);
-  console.log(param);
-
-  console.log(allChat)
-  const { allMessage, errorAllMessage, loadingAllMessage } = useSubscriptionMessage(allChat[0]?.chat_id);
+  const { InsertMessage } = useInsertMessage();
+  const { allMessage, errorAllMessage, loadingAllMessage } =
+    useSubscriptionMessage(data?.chat?.id);
+  console.log(allMessage);
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
-  const onSubmit = () => {
+  const onClick = () => {
+    console.log(value, user.role, data.chat.id);
+
     InsertMessage({
       variables: {
-        // chat_id: allChat[0]?.chat_id,
+        chat_id: data?.chat?.id,
         message: value,
         type: user.role,
       },
     });
+    setValue("");
   };
 
   return (
-    <>
-      <Typography gutterBottom variant="h4" component="div">
-      You have not started a chat, Please click booking on building page
-      </Typography>
-      <Container className={styles.container}>
-        <Box> {allMessage.map((message) => (
-          <Message data={message}/> 
-          ))}
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        // alignItems: "center",
+        justifyContent: "flex-end",
+        // color: "#0000",
+        width: "50%",
+        backgroundColor: "#ffff",
+        borderRadius: 10,
+        padding: (5, 5, 5, 5),
+      }}
+    >
+      
+      {data?.chat?.length === 0 && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            width: "25%",
+            height: "25%",
+            backgroundColor: "#ffff",
+            borderRadius: 10,
+            border: " 1px solid #ABABB1",
+          }}
+        >
+          <Typography gutterBottom variant="h4" color="black" component="div">
+            You have not started a chat, Please click booking on building page
+          </Typography>
         </Box>
-        <Box sx={{ borderRadius: "25px", display: "flex" }}>
-          <form onSubmit={onSubmit}>
-            <InputBase
-              // sx={{ ml: 1, flex: 1 }}
-              value={value}
-              onChange={onChange}
-              placeholder="..."
-              onSubmit={onSubmit}
-            />
-            <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-              <SendIcon />
-            </IconButton>
-          </form>
-        </Box>
-      </Container>
-    </>
+      )}
+      <Box sx={{ overflow: 'auto', height: "80vh" }}>
+        {allMessage?.ofspace_message.map((message) => (
+          <Message data={message} key={message.id} />
+        ))}
+      </Box>
+      <Box
+        sx={{
+          borderRadius: "25px",
+          display: "flex",
+          color: "black",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <InputBase
+          // sx={{ ml: 1, flex: 1 }}
+          value={value}
+          onChange={onChange}
+          placeholder="Enter a message"
+          sx={{ color: "black" }}
+        />
+        <IconButton
+          onClick={onClick}
+          type="button"
+          sx={{ p: "10px", color: "black" }}
+          aria-label="search"
+        >
+          <SendIcon />
+        </IconButton>
+      </Box>
+    </Container>
   );
 }
--- src/components/chat/message.jsx
+++ src/components/chat/message.jsx
@@ -4,30 +4,52 @@ import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
 export default function Chat(data) {
+  console.log(data);
   const user = useSelector((state) => state.persistedReducer.user);
   console.log(user);
 
   return (
     <>
-      {data.type === user.role && (
-        <Box sx={{display: "flex", flexDirection:"column", alignItems:"flex-end"}}>
-          <Typography gutterBottom variant="body1" component="div">
-            {data.message}
-          </Typography>
-          <Typography gutterBottom variant="body2" component="div">
-            {data.updated_at}
-          </Typography>
+      {data.data.type !== user.role && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-end",
+            color: "black",
+            padding: 1
+          }}
+        >
+          <Box sx={{ borderRadius: "5px", bgcolor: "#4A598E", padding: 2 }}>
+            <Typography gutterBottom variant="body1" component="div">
+              {data.data.message}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="div">
+              {data.data.updated_at}
+            </Typography>
+          </Box>
         </Box>
       )}
-      {data.type !== user.role && 
-        <Box sx={{display: "flex", flexDirection:"column", alignItems:"flex-start"}}>
-          <Typography gutterBottom variant="body1" component="div">
-            {data.message}
-          </Typography>
-          <Typography gutterBottom variant="body2" component="div">
-            {data.updated_at}
-          </Typography>
-        </Box>}
+      {data.data.type === user.role && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            color: "black",
+            padding: 1
+          }}
+        >
+          <Box sx={{ borderRadius: "5px", bgcolor: "#508BAE", padding: 2 }}>
+            <Typography gutterBottom variant="body1" component="div">
+              {data.data.message}
+            </Typography>
+            <Typography gutterBottom variant="body2" component="div">
+              {data.data.updated_at}
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </>
   );
 }
