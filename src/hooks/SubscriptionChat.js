da1e2f0ac401ed8ad1cf668d8f066bc1dadf3243
Cendekia Astian <cendiastian@gmail.com>
2022-01-19 11:02:11 +0700

    update Subs Chat hooks

--- src/hooks/SubscriptionChat.js
+++ src/hooks/SubscriptionChat.js
@@ -3,10 +3,17 @@ import { GetChat } from "../graphql/subs";
 
 export function useSubscriptionChat(param) {
   console.log(param)
+  let obj;
+  if (param.role === "customer") {
+    obj = { "customer_id": { "_eq": param.id } };
+  } else {
+    obj = { "consultan_id": { "_eq": param.id } };
+  }
+  console.log(obj)
   const {
     data: allChat,
     loading: loadingAllChat,
     error: errorAllChat,
-  } = useSubscription(GetChat, { variables: { where: param } });
+  } = useSubscription(GetChat, { variables: { where: obj } });
   return { allChat, loadingAllChat, errorAllChat };
 }
