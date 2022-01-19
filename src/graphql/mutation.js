982a3bf88de5f7df653e3435791aa9d1ab4f56e4
Cendekia Astian <cendiastian@gmail.com>
2022-01-19 11:04:29 +0700

    update mutation query

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 const insertChat = gql`
-  mutation MyMutation($customer_id: Int = "", $consultan_id: Int = "") {
+  mutation insertChat($customer_id: Int = "", $consultan_id: Int = "") {
     insert_ofspace_chat_one(
       object: { consultan_id: $consultan_id, customer_id: $customer_id }
     ) {
@@ -14,14 +14,18 @@ const insertChat = gql`
 `;
 
 const insertMessage = gql`
-  mutation MyMutation(
-    $chat_id: uuid = ""
-    $message: String = ""
-    $type: String = ""
-  ) {
+  mutation insertMessage($chat_id: uuid!, $message: String!, $type: String!) {
     insert_ofspace_message_one(
       object: { chat_id: $chat_id, message: $message, type: $type }
-    )
+    ) {
+      id
+    }
   }
 `;
-export { insertChat, insertMessage };
+const deleteChat = gql`
+  mutation deleteChat($id: uuid = "") {
+    delete_ofspace_chat_by_pk(id: $id)
+  } 
+`;
+
+export { insertChat, insertMessage, deleteChat };
