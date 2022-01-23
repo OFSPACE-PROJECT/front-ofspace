7aea0b20eff037a50b4e79eed7e48ec6e267f9be
Cendekia Astian <cendiastian@gmail.com>
2022-01-23 11:17:58 +0700

    forgot commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import User from "./pages/user/customer";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import PrivateRoute from "./route/private";
 import Profile from "./components/user/profil";
+import Booking from "./components/booking/listBooking";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { Box, CircularProgress } from "@mui/material";
+import { useSubscriptionChat } from "./hooks/SubscriptionChat";
 
 const darkTheme = createTheme({
   components: {
@@ -57,22 +62,69 @@ const darkTheme = createTheme({
 });
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [log, setLog] = useState(false);
+  const user = useSelector((state) => state.persistedReducer.user);
+  useEffect(() => {
+    if (user.token) {
+      console.log(loading + user)
+      setLog(true)
+      setLoading(false)
+    } else{
+      console.log(loading + user)
+      setLoading(false)
+    }
+  }, [user]);
+  const { allChat, errorAllChat, loadingAllChat } = useSubscriptionChat(user);
+  useEffect(() => {
+    if (!loadingAllChat) {
+      setLoading(false)
+    }
+  }, [loadingAllChat]);
+
+  console.log(allChat);
+  console.log(allChat?.ofspace_chat);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Router>
         <div className="App">
+        {loading && (
+            <Box
+              sx={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CircularProgress
+                style={{ width: "200px", height: "200px", color: "#white" }}
+              />
+            </Box>
+          )}
+
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/user" element={<PrivateRoute />}>
               <Route path="" element={<User />}>
                 <Route index element={<Profile />} />
-                <Route path="booking" element={<Profile />} />
+                <Route path="booking" element={<Booking />} />
                 <Route path="setting" element={<Profile />} />
               </Route>
             </Route>
           </Routes>
-          <Chat />
+          { log && (
+            <Chat user={user} handleOpen={handleOpen} open={open} handleClose={handleClose} errorAllChat={errorAllChat} allChat={allChat}/>
+          )
+          }
         </div>
       </Router>
     </ThemeProvider>
