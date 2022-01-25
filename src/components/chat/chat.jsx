a56619a26735da31181018338b15e5bfd4c4a3c1
Cendekia Astian <cendiastian@gmail.com>
2022-01-25 15:32:22 +0700

    modify props, date type, flow, etc

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import {
   Typography,
-  Container,
   InputBase,
   IconButton,
   CircularProgress,
@@ -14,7 +13,7 @@ import { useSubscriptionMessage } from "../../hooks/SubscriptionMessage";
 
 export default function Chat(data) {
   const user = data.user;
-  const chat = data.chat[0];
+  const chat = data.chat;
   const [value, setValue] = useState("");
   const { InsertMessage } = useInsertMessage();
   const { allMessage, errorAllMessage, loadingAllMessage } =
@@ -22,34 +21,24 @@ export default function Chat(data) {
 
   const onChange = (e) => {
     setValue(e.target.value);
+    console.log(errorAllMessage);
   };
   const onClick = () => {
     console.log(value, user.role, chat.id);
-
-    InsertMessage({
-      variables: {
-        chat_id: chat?.id,
-        message: value,
-        type: user.role,
-      },
-    });
-    setValue("");
+    if (value.length !== 0) {
+      InsertMessage({
+        variables: {
+          chat_id: chat?.id,
+          message: value,
+          type: user.role,
+        },
+      });
+      setValue("");
+    }
   };
 
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        // alignItems: "center",
-        justifyContent: "flex-end",
-        // color: "#0000",
-        width: "50%",
-        backgroundColor: "#ffff",
-        borderRadius: 10,
-        padding: (5, 5, 5, 5),
-      }}
-    >
+    <>
       {loadingAllMessage && (
         <Box
           sx={{
@@ -63,83 +52,65 @@ export default function Chat(data) {
           />
         </Box>
       )}
-
-      {errorAllMessage && (
+      {!loadingAllMessage && (
         <>
-          {data?.chat?.length === 0 ? (
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                textAlign: "center",
-                // width: "25%",
-                // height: "25%",
-                backgroundColor: "#ffff",
-                // borderRadius: 10,
-                // border: " 1px solid #ABABB1",
-              }}
-            >
-              <Typography
-                gutterBottom
-                variant="h4"
-                color="black"
-                component="div"
+          {errorAllMessage && (
+            <>
+              <Box
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#ffff",
+                }}
               >
-                You have not started a chat, Please click booking on building
-                page
-              </Typography>
-            </Box>
-          ):(
-          <Box
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "#ffff",
-            }}
-          >
-            <Typography gutterBottom variant="h4" color="black" component="div">
-              Something Gone Wrong
-            </Typography>
-          </Box>
+                <Typography
+                  gutterBottom
+                  variant="h4"
+                  color="black"
+                  component="div"
+                >
+                  Something Gone Wrong
+                </Typography>
+              </Box>
+            </>
+          )}
+          {data?.chat?.length !== 0 && (
+            <>
+              <Box sx={{ overflow: "auto", height: "80vh" }}>
+                {allMessage?.ofspace_message.map((message) => (
+                  <Message data={message} key={message.id} user={user} />
+                ))}
+              </Box>
+              <Box
+                sx={{
+                  borderRadius: "25px",
+                  display: "flex",
+                  color: "black",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <InputBase
+                  value={value}
+                  onChange={onChange}
+                  placeholder="Enter a message"
+                  sx={{ color: "black" }}
+                />
+                <IconButton
+                  onClick={onClick}
+                  type="button"
+                  sx={{ p: "10px", color: "black" }}
+                  aria-label="search"
+                >
+                  <SendIcon />
+                </IconButton>
+              </Box>
+            </>
           )}
         </>
       )}
-      {data?.chat?.length !== 0 && (
-        <>
-          <Box sx={{ overflow: "auto", height: "80vh" }}>
-            {allMessage?.ofspace_message.map((message) => (
-              <Message data={message} key={message.id} user={user} />
-            ))}
-          </Box>
-          <Box
-            sx={{
-              borderRadius: "25px",
-              display: "flex",
-              color: "black",
-              width: "100%",
-              justifyContent: "space-between",
-            }}
-          >
-            <InputBase
-              value={value}
-              onChange={onChange}
-              placeholder="Enter a message"
-              sx={{ color: "black" }}
-            />
-            <IconButton
-              onClick={onClick}
-              type="button"
-              sx={{ p: "10px", color: "black" }}
-              aria-label="search"
-            >
-              <SendIcon />
-            </IconButton>
-          </Box>
-        </>
-      )}
-    </Container>
+    </>
   );
 }
--- src/components/chat/modal.jsx
+++ src/components/chat/modal.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import ChatIcon from "@mui/icons-material/Chat";
-import { Typography, Modal, Fab } from "@mui/material";
+import { Typography, Modal, Fab, Container } from "@mui/material";
 import Chat from "./chat";
 export default function ModalChat(props) {
-  const user = props.user
+  const user = props.user;
 
   return (
     <>
@@ -20,7 +20,8 @@ export default function ModalChat(props) {
         }}
         color="primary"
         aria-label="add"
-        onClick={props.handleOpen}>
+        onClick={props.handleOpen}
+      >
         <ChatIcon />
         Live Chat
       </Fab>
@@ -58,7 +59,52 @@ export default function ModalChat(props) {
           borderRadius: 10,
         }}
       >
-        {props.allChat?.ofspace_chat && <Chat chat={props.allChat?.ofspace_chat} user={user}/>}
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            // alignItems: "center",
+            justifyContent: "flex-end",
+            // color: "#0000",
+            width: "50%",
+            backgroundColor: "#ffff",
+            borderRadius: 10,
+            padding: (5, 5, 5, 5),
+          }}
+        >
+          {props.allChat?.ofspace_chat.length === 0 && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                textAlign: "center",
+                // width: "25%",
+                // height: "25%",
+                backgroundColor: "#ffff",
+                // borderRadius: 10,
+                // border: " 1px solid #ABABB1",
+              }}
+            >
+              <Typography
+                gutterBottom
+                variant="h4"
+                color="black"
+                component="div"
+              >
+                You have not started a chat, Please click booking on building
+                page
+              </Typography>
+            </Box>
+          )}
+          {props.allChat?.ofspace_chat.length !== 0 && (
+            <Chat
+              chat={props.allChat?.ofspace_chat[0]}
+              user={user}
+              setLoading={props.setLoading}
+            />
+          )}
+        </Container>
       </Modal>
     </>
   );
